58e59e74c44e308b2a19fd5e69c7fb2ecb3fbecc
Jakub Pokrywka <jakubpokrywka@gmail.com>
2022-11-04 09:53:52 +0000

    a

---
+++ tfidf/1_save.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from stopwords import STOPWORDS
+
+DATA_PROCESSED = 'DATA_PROCESSED'
+
+
+DATA_DIR = '../../data/poleval-passage-retrieval'
+
+
+df_passages_wiki = pd.read_json(DATA_DIR + '/wiki-trivia/passages.jl', lines=True)
+df_passages_wiki['passage_id'] = df_passages_wiki['meta'].apply(lambda x : x['passage_id'])
+df_passages_wiki['article_id'] = df_passages_wiki['meta'].apply(lambda x : x['article_id'])
+df_passages_wiki['set'] = df_passages_wiki['article_id'].astype(str) + '-' + df_passages_wiki['passage_id'].astype(str)
+
+df_passages_legal = pd.read_json(DATA_DIR + '/legal-questions/passages.jl', lines=True)
+df_passages_allegro = pd.read_json(DATA_DIR + '/allegro-faq/passages.jl', lines=True)
+
+df_queries_train = pd.read_csv('../../data/2022-passage-retrieval/train/in.tsv', sep =  '\t', names = ('source', 'text'))
+df_queries_dev = pd.read_csv('../../data/2022-passage-retrieval/dev-0/in.tsv', sep =  '\t', names = ('source', 'text'))
+df_queries_test_wiki = pd.read_csv('../../data/2022-passage-retrieval/test-A-wiki/in.tsv', sep =  '\t', names = ('source', 'text'))
+df_queries_test_legal = pd.read_csv('../../data/2022-passage-retrieval/test-A-legal/in.tsv', sep =  '\t', names = ('source', 'text'))
+df_queries_test_allegro = pd.read_csv('../../data/2022-passage-retrieval/test-A-allegro/in.tsv', sep =  '\t', names = ('source', 'text'))
+
+
+vectorizer_wiki = TfidfVectorizer(stop_words=STOPWORDS) 
+vectorizer_wiki.fit( list(df_passages_wiki['text']) + list(df_queries_train['text']) )
+
+vectorizer_all = TfidfVectorizer(stop_words=STOPWORDS) 
+corpora_all=  (list(df_passages_wiki['text']) + list(df_queries_train['text'])  
+                + list(df_queries_dev['text'])  
+                + list(df_queries_test_wiki['text'])  
+                + list(df_queries_test_allegro['text'])  
+                + list(df_queries_test_legal['text']) )
+
+vectorizer_all.fit(corpora_all)
+
+
+passages_text_transformed_wiki = vectorizer_wiki.transform(df_passages_wiki['text'])
+passages_text_transformed_legal = vectorizer_all.transform(df_passages_legal['text'])
+passages_text_transformed_allegro = vectorizer_all.transform(df_passages_allegro['text'])
+
+
+
+queries_text_transformed_train = vectorizer_wiki.transform(df_queries_train['text'])
+queries_text_transformed_dev = vectorizer_wiki.transform(df_queries_dev['text'])
+queries_text_transformed_test_wiki = vectorizer_wiki.transform(df_queries_test_wiki['text'])
+queries_text_transformed_test_legal = vectorizer_all.transform(df_queries_test_legal['text'])
+queries_text_transformed_test_allegro = vectorizer_all.transform(df_queries_test_allegro['text'])
+
+#sims = cosine_similarity(queries_text_transformed, passages_text_transformed_wiki)
+
+with open(f'{DATA_PROCESSED}/vectorizer_wiki.pkl','wb') as f_out:
+    pickle.dump(vectorizer_wiki,f_out)
+
+with open(f'{DATA_PROCESSED}/vectorizer_all.pkl','wb') as f_out:
+    pickle.dump(vectorizer_all,f_out)
+
+with open(f'{DATA_PROCESSED}/passages_text_transformed_wiki.pkl','wb') as f_out:
+    pickle.dump(passages_text_transformed_wiki,f_out)
+
+with open(f'{DATA_PROCESSED}/passages_text_transformed_legal.pkl','wb') as f_out:
+    pickle.dump(passages_text_transformed_legal,f_out)
+
+with open(f'{DATA_PROCESSED}/passages_text_transformed_allegro.pkl','wb') as f_out:
+    pickle.dump(passages_text_transformed_allegro,f_out)
+
+with open(f'{DATA_PROCESSED}/queries_text_transformed_train.pkl','wb') as f_out:
+    pickle.dump(queries_text_transformed_train,f_out)
+
+with open(f'{DATA_PROCESSED}/queries_text_transformed_dev.pkl','wb') as f_out:
+    pickle.dump(queries_text_transformed_dev,f_out)
+
+with open(f'{DATA_PROCESSED}/queries_text_transformed_test_wiki.pkl','wb') as f_out:
+    pickle.dump(queries_text_transformed_test_wiki,f_out)
+
+with open(f'{DATA_PROCESSED}/queries_text_transformed_test_legal.pkl','wb') as f_out:
+    pickle.dump(queries_text_transformed_test_legal,f_out)
+
+with open(f'{DATA_PROCESSED}/queries_text_transformed_test_allegro.pkl','wb') as f_out:
+    pickle.dump(queries_text_transformed_test_allegro,f_out)
+
+with open(f'{DATA_PROCESSED}/df_passages_wiki.pkl','wb') as f_out:
+    pickle.dump(df_passages_wiki,f_out)
+
+with open(f'{DATA_PROCESSED}/df_passages_legal.pkl','wb') as f_out:
+    pickle.dump(df_passages_legal,f_out)
+
+with open(f'{DATA_PROCESSED}/df_passages_allegro.pkl','wb') as f_out:
+    pickle.dump(df_passages_allegro,f_out)
---
+++ tfidf/2.py
@@ -0,0 +1,70 @@
+import pickle
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+DATA_DIR = 'DATA_PROCESSED'
+
+
+with open(DATA_DIR + '/vectorizer_wiki.pkl','rb') as f_out:
+    vectorizer_wiki = pickle.load(f_out)
+
+with open(DATA_DIR + '/vectorizer_all.pkl','rb') as f_out:
+    vectorizer_all = pickle.load(f_out)
+
+with open(DATA_DIR + '/passages_text_transformed_wiki.pkl','rb') as f_out:
+    passages_text_transformed_wiki = pickle.load(f_out)
+
+with open(DATA_DIR + '/passages_text_transformed_legal.pkl','rb') as f_out:
+    passages_text_transformed_legal = pickle.load(f_out)
+
+with open(DATA_DIR + '/passages_text_transformed_allegro.pkl','rb') as f_out:
+    passages_text_transformed_allegro = pickle.load(f_out)
+
+with open(DATA_DIR + '/queries_text_transformed_dev.pkl','rb') as f_out:
+    queries_text_transformed_dev = pickle.load(f_out)
+
+with open(DATA_DIR + '/queries_text_transformed_test_wiki.pkl','rb') as f_out:
+    queries_text_transformed_test_wiki = pickle.load(f_out)
+
+with open(DATA_DIR + '/queries_text_transformed_test_legal.pkl','rb') as f_out:
+    queries_text_transformed_test_legal = pickle.load(f_out)
+
+with open(DATA_DIR + '/queries_text_transformed_test_allegro.pkl','rb') as f_out:
+    queries_text_transformed_test_allegro = pickle.load(f_out)
+
+with open(DATA_DIR + '/queries_text_transformed_train.pkl','rb') as f_out:
+    queries_text_transformed_train = pickle.load(f_out)
+
+with open(DATA_DIR + '/df_passages_wiki.pkl','rb') as f_out:
+    df_passages_wiki = pickle.load(f_out)
+
+with open(DATA_DIR + '/df_passages_legal.pkl','rb') as f_out:
+    df_passages_legal = pickle.load(f_out)
+
+with open(DATA_DIR + '/df_passages_allegro.pkl','rb') as f_out:
+    df_passages_allegro = pickle.load(f_out)
+
+NR_OF_INDICES=10
+def run(queries_text_transformed, df_passages, passages_text_transformed, f_out_path):
+    top10_indices = []
+    bs_size = 500
+    for bs_in in tqdm(range(0, queries_text_transformed.shape[0], bs_size)):
+        queries_batch = queries_text_transformed[bs_in: bs_in +bs_size]
+        sims = cosine_similarity(queries_batch, passages_text_transformed)
+        top10_indices_batch = np.argsort(-sims)[:,:NR_OF_INDICES].tolist()
+        top10_indices += top10_indices_batch
+
+    f_out = open(f_out_path, 'w')
+    for i in tqdm(range(len(top10_indices))):
+        o = df_passages.iloc[top10_indices[i]]['id'].tolist()
+        o = [str(a) for a in o]
+        f_out.write('\t'.join(o) + '\n')
+    f_out.close()
+
+run(queries_text_transformed_train, df_passages_wiki, passages_text_transformed_wiki,  '../../data/2022-passage-retrieval/train/out.tsv')
+run(queries_text_transformed_dev, df_passages_wiki, passages_text_transformed_wiki, '../../data/2022-passage-retrieval/dev-0/out.tsv')
+run(queries_text_transformed_test_wiki, df_passages_wiki, passages_text_transformed_wiki,  '../../data/2022-passage-retrieval/test-A-wiki/out.tsv')
+run(queries_text_transformed_test_legal, df_passages_legal, passages_text_transformed_legal,  '../../data/2022-passage-retrieval/test-A-legal/out.tsv')
+run(queries_text_transformed_test_allegro, df_passages_allegro, passages_text_transformed_allegro,  '../../data/2022-passage-retrieval/test-A-allegro/out.tsv')
